e2974237047c5f867100abd3b3f1429ab3267807
Anoop P Oommen <aronstone.stone@gmail.com>
2018-07-28 09:41:30 +0530

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <h1>Registration</h1>
+    <form v-on:submit.prevent="addTicket">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Email ID:</label>
+            <input type="text" class="form-control" v-model="reg.email"/>
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Password</label>
+              <input typ="text" class="form-control" v-model="reg.description"></textarea>
+            </div>
+          </div>
+        </div><br />
+        <div class="form-group">
+          <button class="btn btn-primary">Register</button>
+        </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+          reg: {}
+        }
+    },
+   methods: {
+       addTicket() {
+             let uri = 'http://localhost:4000/tickets/add';
+            this.axios.post(uri, this.reg).then((response) => {
+               console.log(response);
+            });
+       }
+    }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+let router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+    },
+  ]
+})
+
+export default router
